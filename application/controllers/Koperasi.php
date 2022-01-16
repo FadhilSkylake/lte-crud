25b3dbdd2fc253e458b277c3ae738801e889eff9
FadhilSkylake <fadhil1023@gmail.com>
2022-01-16 21:29:49 +0700

    commit

---
+++ application/controllers/Koperasi.php
@@ -0,0 +1,15 @@
+<?php
+defineD('BASEPATH') OR exit('No direct script access allowed');
+
+class Koperasi extends CI_Controller {
+    
+    // public function __construct() {
+	// 	parent::__construct();
+	// 	$this->load->model('M_main');
+	// }
+
+    public function index() {
+        $this->load->view('front/koperasi');
+    }
+    
+    }
