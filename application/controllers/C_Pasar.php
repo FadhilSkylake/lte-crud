587d894872e053fc7ff572e31c47beecc3e80c9e
kurosagi122 <riyadhfauzan12@gmail.com>
2022-01-18 14:57:20 +0700

    commit

--- application/controllers/C_Pasar.php
+++ application/controllers/C_Pasar.php
@@ -1,7 +1,7 @@
 <?php
 defineD('BASEPATH') OR exit('No direct script access allowed');
 
-class C_Pasar extends CI_Controller {
+class C_pasar extends CI_Controller {
     
     // public function __construct() {
 	// 	parent::__construct();
